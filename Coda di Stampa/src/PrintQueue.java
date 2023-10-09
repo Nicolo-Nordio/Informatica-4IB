4de65e245186642b681edb301d3c2146c2cfe99a
Nordio Nicolo <nicolo.nordio@cz.lan>
2023-10-09 13:31:45 +0200

    Terzo commit

--- Coda di Stampa/src/PrintQueue.java
+++ Coda di Stampa/src/PrintQueue.java
@@ -1,63 +1,50 @@
+import java.io.IOException;
+
 public class PrintQueue{
-    private FileTesto file;
     private Node head;
+    private Node back;
 
     public static class Node{
-        private char value;
+        private FileTesto value;
         private Node next;
 
-        Node(char value){
+        Node(FileTesto value){
             this.value = value;
             next = null;
         }
-    }
-
-    PrintQueue(FileTesto file){
-        this.file = file;
-    }
-
-    public void push(char value){
-        Node newNode = new Node(value);
-        newNode.next = head;
-        head = newNode;
-    }
-
-    public void pop(){
-        if (head != null) {
-            head = head.next;
+        @Override
+        public String toString(){
+            try {
+                return value.read();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
+    }
+
+    PrintQueue(){
+        this.head = null;
+        this.back = null;
+    }
+
+    public void push(FileTesto carattere){
+        Node newNode = new Node(carattere);
+        if (head==null){
+            newNode.next = head;
+            head = newNode;
+        }
+        newNode.next = back;
+        back = newNode;
+    }
+
+    public void pop(FileTesto file){
+        if (isEmpty()) {
+           throw new IllegalStateException("la pila è vuota");
+        }
+        head = head.next;
     }
 
     public boolean isEmpty(){
         return head == null;
-    }
-
-    public void verifica(){
-        Pila stack = new Pila("");
-        for (int i = 0; i < espressione.length(); i++) {
-            char carattere = espressione.charAt(i);
-            if (carattere == '{' || carattere == '[' || carattere == '(') {
-                stack.push(carattere);
-            } else if (carattere == '}' || carattere == ']' || carattere == ')') {
-                if (stack.isEmpty()) {
-                    System.out.println("Parentesi non bilanciate");
-                    return;
-                }
-                char parentesiAperta = stack.head.value;
-                if ((carattere == '}' && parentesiAperta == '{') ||
-                        (carattere == ']' && parentesiAperta == '[') ||
-                        (carattere == ')' && parentesiAperta == '(')) {
-                    stack.pop();
-                } else {
-                    System.out.println("Parentesi non bilanciate");
-                    return;
-                }
-            }
-        }
-        if (stack.isEmpty()) {
-            System.out.println("Parentesi bilanciate");
-        } else {
-            System.out.println("Parentesi non bilanciate");
-        }
     }
 }
