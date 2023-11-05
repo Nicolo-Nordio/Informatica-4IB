a9f5612c73714466939c521ab268b4019046520e
Nicolo-Nordio <nordionicolo14@gmail.com>
2023-11-05 16:39:17 +0100

    Aggiunta classe Main

--- ClasseBtree/src/BTree.java
+++ ClasseBtree/src/BTree.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Stack;
 
 public class BTree {
-    private List<Integer> treeList;
+    protected List<Integer> treeList;
 
     public BTree() {
         this.treeList = new ArrayList<>();
@@ -32,22 +32,6 @@ public class BTree {
                 stack.push(leftChildIndex);
             }
         }
-
         return result;
-    }
-
-    public static void main(String[] args) {
-        BTree bTree = new BTree();
-        bTree.treeList.add(1);
-        bTree.treeList.add(2);
-        bTree.treeList.add(3);
-        bTree.treeList.add(4);
-        bTree.treeList.add(5);
-        bTree.treeList.add(6);
-        bTree.treeList.add(7);
-        bTree.treeList.add(8);
-        bTree.treeList.add(9);
-        bTree.treeList.add(10);
-        System.out.println(bTree.preOrder());
     }
 }
