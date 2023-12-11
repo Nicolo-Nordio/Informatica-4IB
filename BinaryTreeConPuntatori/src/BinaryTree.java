b03cb3f5f613285be41c281fcd6c27d994f38c4a
Nordio Nicolo <nicolo.nordio@cz.lan>
2023-12-11 13:45:59 +0100

    Andando avanti

--- BinaryTreeConPuntatori/src/BinaryTree.java
+++ BinaryTreeConPuntatori/src/BinaryTree.java
@@ -1,43 +1,73 @@
-public class BinaryTree {
+import java.util.ArrayList;
 
-    static public class Node{
+public class BinaryTree<E> {
+
+    static public class Node<E>{
         private int value;
-        private Node left;
-        private Node right;
+        private Node<E> left;
+        private Node<E> right;
 
         Node(int value){
-            left = null;
-            right = null;
+            this.left = null;
+            this.right = null;
             value = 0;
         }
 
+        /**
+         * Vari getter
+         * @return
+         */
         public int getValue() {
             return value;
         }
-
-        public Node getLeft() {
+        public Node<E> getLeft() {
             return left;
         }
-
-        public Node getRight() {
+        public Node<E> getRight() {
             return right;
+        }
+
+        /**
+         * Vari setter
+         * @param
+         */
+        public void setValue(int value) {
+            this.value = value;
+        }
+        public void setRight(Node<E> right) {
+            this.right = right;
+        }
+        public void setLeft(Node<E> left) {
+            this.left = left;
         }
     }
 
-    private Node root;
+    private Node<E> root;
+    private ArrayList<Node> nodi = new ArrayList<>();
 
     public BinaryTree() {
         root = null;
     }
- // Da capire
+
     public BinaryTree(Node root){
         this.root = root;
     }
 
+    /**
+     * Restituisce una rappresentazione testuale dell'albero binario
+     * @param r
+     * @return
+     */
     public String print(Node r){
         return
     }
 
+    /**
+     * Cerca se il valore val appartiene o meno all’albero binario usando una visita in ordine anticipato
+     * @param value
+     * @param current
+     * @return
+     */
     public boolean search(int value, Node current) {
         if (current == null) {
             return false;
@@ -54,8 +84,22 @@ public class BinaryTree {
         }
     }
 
+    /**
+     * Conta il numero di foglie dell’albero
+     * @param r
+     * @return nFoglie;
+     */
     public int contaFoglie(Node r){
-
+        int nFoglie = 0;
+        //Inserire codice
+        return nFoglie;
     }
 
+    public Node getRoot() {
+        return root;
+    }
+
+    public void createBTree(ArrayList<Node> nodi){
+
+    }
 }
--- BinaryTreeConPuntatori/src/Main.java
+++ BinaryTreeConPuntatori/src/Main.java
@@ -1,7 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-
         BinaryTree AB = new BinaryTree();
 
+        // ((( 30 ) 20 ) 10 (( 50 ) 40 ( 60 ( 70 ))))
+        System.out.println(AB.print(AB.getRoot()));
+        // esiste 20 = true
+        System.out.println("esiste 20 = " + AB.search(20, AB.getRoot()));
+        // esiste 25 = false
+        System.out.println("esiste 25 = " + AB.search(25, AB.getRoot()));
+        // numero foglie = 3
+        System.out.println("numero foglie = " + AB.contaFoglie(AB.getRoot()));
     }
-}
+}sSSS
